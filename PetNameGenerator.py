537ea90e3357a354b81089edf5438c90d8f888fe
Blex Olonde <86054041+blexolonde@users.noreply.github.com>
2025-03-18 10:23:11 +0300

    Add files via upload

---
+++ PetNameGenerator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Welcome to the Pet Name Generator.")
+#2. Ask the user who his fav super hero is
+super_hero = input("What's the name of your favorite super hero?\n") 
+#3. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + super_hero + " " + city)
+
+
+
+
+
---
+++ Readme.md
@@ -0,0 +1,1 @@
+This small program will give you a suggested pet name for you having in mind your favorite supper hero and the city you grew up in
